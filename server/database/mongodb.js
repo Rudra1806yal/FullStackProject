e56b7e0f9594c295d28c3d25ae709f20dae3088d
Rudra Juyal <rudrajuyal42@gmail.com>
2026-07-20 10:48:37 +0530

    changes in mongodb

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose"
 
 async function connectDb(){
     try{
-        await mongoose.connect("mongodb://localhost:27017/employee_management_system")
+        await mongoose.connect("mongodb://fullStackDeveloper:rudra1234@ac-7zmjdzq-shard-00-00.bzunkgz.mongodb.net:27017,ac-7zmjdzq-shard-00-01.bzunkgz.mongodb.net:27017,ac-7zmjdzq-shard-00-02.bzunkgz.mongodb.net:27017/?ssl=true&replicaSet=atlas-v88o7k-shard-0&authSource=admin&appName=Cluster0")
+
         console.log('database connected successfully...')
     }catch(error){
         console.log('database has not been connected', error)
